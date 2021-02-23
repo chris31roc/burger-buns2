d6d91d3f6942bcff1a7aed38b65a92c2536f767c
chris31roc <chris31roc@yahoo.com>
2021-02-23 13:23:15 -0700

    updating db folder with new schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Chicken Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Salmon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pickled Jalapeño Cheddar Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smothered Turkey Burger', false)
